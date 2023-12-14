7a5b9c8cf037c70a7dc573fb500213bfb5846891
Mayank Shukla <117716383+mayankshukla-20@users.noreply.github.com>
2023-12-14 17:13:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let msg1 = prompt("Enter first number");
 let n1 = parseInt(msg1);
 let msg2 = prompt("Enter second number");
 let n2 = parseInt(msg2);
-
-let result = n1*2 + n2;
 alert("The final result is:" + result);
+function calc(n1,n2){
+	let result = 2*n1 + n2;
+	alert("The final result is:" + result)
+}
